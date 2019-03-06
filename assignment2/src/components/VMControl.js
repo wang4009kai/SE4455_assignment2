7b346d1f419ebc5f62d2089df2db693794f75d24
Kai Wang <kaiwang@Kais-MacBook-Pro.local>
2019-03-06 15:06:34 -0500

    added the VM management part

--- assignment2/src/components/VMControl.js
+++ assignment2/src/components/VMControl.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Grid, Button, Header, Form, Radio } from 'semantic-ui-react'
+import { Button, Header, Form, Radio, Popup, Table, Segment } from 'semantic-ui-react'
 
 class VMControl extends Component {
     constructor(props) {
@@ -9,8 +9,13 @@ class VMControl extends Component {
             email: this.props.username,
             VMs: [],
             cost: 0,
+            showTemplate: false,
+            showCost: false,
         };
+        this.getVMs();
     }
+
+    handleChange = (e, { value }) => this.setState({ value })
 
     getVMs() {
         axios.post("https://localhost:8080/getVM",
@@ -22,75 +27,202 @@ class VMControl extends Component {
     }
 
     getCost() {
-        axios.post("https://localhost:8080/getCost",
-            {
-                user: this.state.email,
-            }).then(function (response) {
-            this.state.cost = response.data.cost;
-        }).catch(e => console.log(e));
+        if (this.state.showCost === true) {
+            this.setState({showCost: false});
+        } else {
+            this.setState({showCost: true});
+            axios.post("https://localhost:8080/getCost",
+                {
+                    user: this.state.email,
+                }).then(function (response) {
+                this.state.cost = response.data.cost;
+            }).catch(e => console.log(e));
+        }
     }
 
-    createVM(id) {
-
+    createVM() {
+        this.setState({action: 'create'});
+        this.setState({showTemplate: true});
     }
 
-    upgradeVM(id) {
-
+    upgradeVM(id, type) {
+        this.setState({value: type});
+        this.setState({action: 'upgrade'});
+        this.setState({focus: id});
+        this.setState({showTemplate: true});
     }
 
     deleteVM(id) {
-
+        axios.post("https://localhost:8080/delete",
+            {
+                user: this.state.email,
+                vm: id,
+            }).then(function (response) {
+                let array = this.state.VMs;
+                let index = this.state.VMs.indexOf(id);
+                array.splice(index, 1);
+                this.setState({VMs: array});
+        }).catch(e => console.log(e));
     }
 
-    VMTemplate(id, type) {
-        return(
-            <Grid.Column>
-                <div>
-                    <Header size='medium'>{id}</Header>
-                    <Header size='medium'>{type}</Header>
-                </div>
-                <div>
-                    <Button color='green' onClick={() => this.createVM(id)}>Create VM</Button>
-                    <Button color='blue' onClick={() => this.upgradeVM(id)}>Modify VM Config</Button>
-                    <Button color='red' onClick={() => this.deleteVM(id)}>Delete VM</Button>
-                </div>
-            </Grid.Column>
-        );
+    startVM(id) {
+        axios.post("https://localhost:8080/start",
+            {
+                user: this.state.email,
+                vm: id,
+            }).then(function (response) {
+            let array = this.state.VMs;
+            let index = this.state.VMs.indexOf(id);
+            array[index].status = 'start';
+            this.setState({VMs: array});
+        }).catch(e => console.log(e));
     }
 
-    VMSelection(type) {
-        return(
-            <Form>
-                <Form.Field>
-                    Selected value: <b>{this.state.value}</b>
-                </Form.Field>
-                <Form.Field>
-                    <Radio
-                        label='Basic Virtual Server Instance - 8 virtual processor cores, 16 GB of virtual RAM, 20 GB
-                        of storage space in the root file system - 5 cents/minute '
-                        name='radioGroup'
-                        value='this'
-                        checked={this.state.value === 'this'}
-                        onChange={this.handleChange}
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <Radio
-                        label='Or that'
-                        name='radioGroup'
-                        value='that'
-                        checked={this.state.value === 'that'}
-                        onChange={this.handleChange}
-                    />
-                </Form.Field>
-            </Form>
-        );
+    stopVM(id) {
+        axios.post("https://localhost:8080/stop",
+            {
+                user: this.state.email,
+                vm: id,
+            }).then(function (response) {
+            let array = this.state.VMs;
+            let index = this.state.VMs.indexOf(id);
+            array[index].status = 'stop';
+            this.setState({VMs: array});
+        }).catch(e => console.log(e));
+    }
+
+    makeChange(){
+        if(this.state.action === 'upgrade') {
+            let array = this.state.VMs;
+            let index = this.state.VMs.indexOf(this.state.focus);
+            if (array[index].type != this.state.value) {
+                axios.post("https://localhost:8080/delete",
+                    {
+                        user: this.state.email,
+                        vm: this.state.focus,
+                        type: this.state.value,
+                    }).then(function (response) {
+
+                    array[index].type = this.state.value;
+                    this.setState({VMs: array});
+                }).catch(e => console.log(e));
+            }
+        } else if (this.state.action === 'create') {
+            axios.post("https://localhost:8080/create",
+                {
+                    user: this.state.email,
+                    vm: this.state.value,
+                }).then(function (response) {
+                let array = this.state.VMs;
+                array.push(response.data)
+                this.setState({VMs: array});
+            }).catch(e => console.log(e));
+        }
+
+        // Reset the state.
+        this.setState({action: null});
+        this.setState({showTemplate:false});
+        this.setState({value: null});
+    }
+
+    myVM() {
+        let container = [];
+        this.state.VMs.map((item, i) => {
+            container.push(
+                <Table.Row>
+                    <Table.Cell>
+                        <Header as='h2' textAlign='center'>
+                            {item.id}
+                        </Header>
+                    </Table.Cell>
+                    <Table.Cell singleLine>{item.type}</Table.Cell>
+                    <Table.Cell singleLine>{item.status}</Table.Cell>
+                    <Table.Cell>
+                        <Button color='blue' onClick={() => this.upgradeVM(item.id)}>Modify VM</Button>
+                    </Table.Cell>
+                    <Table.Cell textAlign='right'>
+                        <Button color='red' onClick={() => this.deleteVM(item.id)}>Delete VM</Button>
+                    </Table.Cell>
+                    <Table.Cell textAlign='right'>
+                        <Button onClick={() => this.startVM(item.id)}>Start VM</Button>
+                    </Table.Cell>
+                    <Table.Cell textAlign='right'>
+                        <Button onClick={() => this.stopVM(item.id)}>Stop VM</Button>
+                    </Table.Cell>
+                </Table.Row>
+            )
+        });
+        return container;
     }
 
     render() {
         return (
             <div>
-                <p>Welcome to Todos List Component!!</p>
+                <div>
+                    <Button onClick={() => this.getCost()}>Get Cost</Button>
+                </div>
+                <Table celled padded>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell singleLine>VM ID</Table.HeaderCell>
+                            <Table.HeaderCell>VM Type</Table.HeaderCell>
+                            <Table.HeaderCell>VM Status</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        {this.myVM()}
+                    </Table.Body>
+                </Table>
+                <br/>
+                <div>
+                    <Button onClick={() => this.createVM()}>Create VM</Button>
+                    { this.state.showCost ? (<Segment>{this.state.cost}</Segment>) : null}
+                </div>
+                <br/>
+                { this.state.showTemplate ? (
+                    <Form>
+                        <Form.Field>
+                            <Popup trigger={
+                                <Radio
+                                    label='Basic Virtual Server Instance'
+                                    name='radioGroup'
+                                    value='basic'
+                                    checked={this.state.value === 'basic'}
+                                    onChange={this.handleChange}
+                                />
+                            } content='8 virtual processor cores, 16 GB of virtual RAM, 20 GB of storage space in the root file
+                    system - 5 cents/minute' />
+                        </Form.Field>
+                        <Form.Field>
+                            <Popup trigger={
+                                <Radio
+                                    label='Large Virtual Server Instance'
+                                    name='radioGroup'
+                                    value='large'
+                                    checked={this.state.value === 'large'}
+                                    onChange={this.handleChange}
+                                />
+                            } content='32 virtual processor cores, 64 GB of virtual RAM, 20 GB of storage space in the root file
+                     system -10 cents/minute' />
+                        </Form.Field>
+                        <Form.Field>
+                            <Popup trigger={
+                                <Radio
+                                    label='Ultra-Large Virtual Server Instance'
+                                    name='radioGroup'
+                                    value='ultra'
+                                    checked={this.state.value === 'ultra'}
+                                    onChange={this.handleChange}
+                                />
+                            } content='128 virtual processor cores, 512 GB of virtual RAM, 40 GB of storage space in the root
+                    file system- 15 cents /minute' />
+                        </Form.Field>
+                        <div>
+                            <Button onClick={() => this.makeChange()}>Submit</Button>
+                        </div>
+                    </Form>
+                ) : null }
             </div>
         )
     }
